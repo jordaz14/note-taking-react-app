a48c3735f5d3efbc6aab787af52df0e65cb71e68
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-07-06 15:13:04 -0400

    Create tri-card page layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,19 @@
 function App() {
   return (
     <>
-      <div>
-        <p className="text-3xl font-bold underline">
-          This is my tailwind branch
-        </p>
+      <div className="flex font-nunito">
+        <nav id="sidebar-left" className="bg-neutral-50 w-[10%] h-screen font">
+          Left Sidebar
+        </nav>
+        <main className="flex-1">
+          <div id="prompt" className="bg-yellow-100">
+            <p className="">Prompt</p>
+          </div>
+          <div id="content">Content</div>
+        </main>
+        <section id="sidebar-right" className="bg-neutral-50 w-[10%] h-screen">
+          Right Sidebar
+        </section>
       </div>
     </>
   );
