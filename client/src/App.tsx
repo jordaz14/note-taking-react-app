a619b1430e4f296756a258abcf8e7755b65ce7c1
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-07-07 21:46:01 -0400

    Add toggle icon to lefthand nav

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
+
 import LeftSidebar from "./components/LeftSidebar";
 import RightSidebar from "./components/RightSidebar";
 
+import threedot from "./assets/threedot.png";
+
 function App() {
+  const [visible, isVisible] = useState(false);
+
   return (
     <>
       <div className="flex font-nunito">
         <LeftSidebar />
-        <div id="leftToggle" className="bg-blue-100">
-          Left Toggle
+        <div id="leftToggle" className="flex flex-col justify-center w-fit">
+          <img src={threedot} alt="" width={20} height={20} />
         </div>
-        <main className="flex-1">
+        <main className="flex-1 bg-blue-100">
           <div id="prompt" className="bg-yellow-100">
             <p className="">Prompt</p>
           </div>
