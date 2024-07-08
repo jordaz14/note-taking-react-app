a323ca43e1a8801fbb705ee2c89d5aaf5d654e4a
Julian <1julianordaz@gmail.com>
2024-07-08 12:02:10 -0400

    Add writing block component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import LeftSidebar from "./components/LeftSidebar";
 import RightSidebar from "./components/RightSidebar";
+import WritingBlock from "./components/WritingBlock";
 
 import threedot from "./assets/threedot.png";
 
@@ -12,15 +13,17 @@ function App() {
     <>
       <div className="flex font-nunito">
         <LeftSidebar />
-        <div id="leftToggle" className="flex flex-col justify-center w-fit">
+        <div id="barToggle" className="flex flex-col justify-center w-fit">
           <img src={threedot} alt="" width={20} height={20} />
         </div>
-        <main className="flex-1 bg-blue-100">
-          <div id="prompt" className="bg-yellow-100">
-            <p className="">Prompt</p>
-          </div>
-          <div id="content">Content</div>
+        <main className="flex-1 bg-blue-100 flex flex-col items-center pt-2 pb-2">
+          <WritingBlock />
+          <WritingBlock />
+          <WritingBlock />
         </main>
+        <div id="barToggle" className="flex flex-col justify-center w-fit">
+          <img src={threedot} alt="" width={20} height={20} />
+        </div>
         <RightSidebar />
       </div>
     </>
---
+++ client/src/components/WritingBlock.tsx
@@ -0,0 +1,18 @@
+function WritingBlock() {
+  return (
+    <div
+      id="content"
+      className="bg-white w-[50%] rounded-md drop-shadow-md hover:drop-shadow-2xl p-2 mb-4"
+    >
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </div>
+  );
+}
+
+export default WritingBlock;
