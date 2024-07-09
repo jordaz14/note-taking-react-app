2f50841920b363b3aa2844acc4349f4a89957827
Julian <1julianordaz@gmail.com>
2024-07-09 12:41:51 -0400

    Create list function, tbu

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,3 @@
-import LeftSidebar from "./components/LeftSidebar";
-import RightSidebar from "./components/RightSidebar";
 import WritingBlock from "./components/WritingBlock";
 import ScratchSpace from "./components/ScratchSpace";
 
@@ -7,7 +5,7 @@ import threedot from "./assets/threedot.png";
 import { useState } from "react";
 
 function App() {
-  const [list, setList] = useState<Number[]>([]);
+  const [list, setList] = useState<Number[]>([0]);
 
   const addToList = () => {
     let tempArr: any[] = list;
@@ -17,7 +15,6 @@ function App() {
 
   return (
     <>
-      <div>WrtBlk</div>
       <div className="flex font-nunito">
         <div
           id="barToggle"
@@ -28,11 +25,16 @@ function App() {
         <main id="canvas" className="flex w-screen h-screen p-4 gap-4">
           <ScratchSpace />
           <div className="flex-[2] bg-blue-100 flex flex-col items-center pt-2 pb-2 gap-2">
-            <WritingBlock />
-            <WritingBlock />
+            <ul>
+              {list.map((item) => (
+                <li>{item.toString()}</li>
+              ))}
+            </ul>
             <div className="flex gap-2">
               <button className="bg-white p-1 font-bold">-</button>
-              <button className="bg-white p-1 font-bold">+</button>
+              <button onClick={addToList} className="bg-white p-1 font-bold">
+                +
+              </button>
             </div>
           </div>
           <ScratchSpace />
